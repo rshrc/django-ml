88446c3cfec1f4766747d950472ed982d580ae34
Rishi Banerjee <rishieric91@gmail.com>
2020-05-16 10:47:30 +0530

    Clean training file

--- predict/train.py
+++ predict/train.py
@@ -3,8 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import pickle
 
-from django.db import models
-
 # Importing the dataset
 dataset = pd.read_csv('data.csv')
 X = dataset.iloc[:, :-1].values
@@ -12,7 +10,8 @@ y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=1 / 3, random_state=0)
 
 # Fitting Simlple Regression Data Set into Training Set
 print("X_test")
@@ -25,10 +24,5 @@ print(y_pred)
 
 filename = 'model.sav'
 
+# Model percistency
 pickle.dump(regressor, open(filename, 'wb'))
-
-print("Loading later")
-
-loaded_model = pickle.load(open(filename, 'rb'))
-result = loaded_model.predict(X_test)
-print(result)
